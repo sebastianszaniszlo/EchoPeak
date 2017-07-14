c0b8763b17b91b9e651354a73244c81223db5ad6
Sebastian Szaniszlo <sebastian.szaniszlo@gmail.com>
2017-07-14 20:49:59 +0100

    Made the board blink while connected to the server

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const johnnyFive = require('johnny-five');
+
 const app = express()
+
+const board = new johnnyFive.Board();
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
+   board.on("ready", function() {
+        var led = new johnnyFive.Led(13);
+
+        led.blink(1000);
+  });
 })
